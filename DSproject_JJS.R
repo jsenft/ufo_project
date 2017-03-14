04e5e5deb455718d89ea7658245b0167d7aef527
John Senft <jsenft2001@yahoo.com>
2017-03-14 12:17:04 -0700

    Finalized project

--- DSproject_JJS.R
+++ DSproject_JJS.R
@@ -12,6 +12,7 @@
 ## https://www.census.gov/library/publications/2011/compendia/statab/131ed/population.html
 ## https://www.kaggle.com/NUFORC/ufo-sightings
 ## http://www.fonz.net/blog/archives/2008/04/06/csv-of-states-and-state-abbreviations/
+## Twitter post search via twitter api
 ##--------------------------------------------
 
 # Load libraries
@@ -29,18 +30,24 @@ trim.trailing <- function (x) sub("\\s+$", "", x)
 
 load_my_csv <- function(filename) {
   stopifnot(!is.null(filename))
-  data = read.csv(filename)
+  data = read.csv(filename, stringsAsFactors = FALSE)
   flog.info("Data has loaded from: %s", filename, name="fp9")
   return(data)
 }
 
-add_tweets <- function(x) {
-  tweets <- searchTwitter('ufo -filter:retweets', n=2000, since=x, lang='en')
+add_tweets <- function(x, y) {
+  setup_twitter_oauth(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+  tweets <- searchTwitter('ufo -filter:retweets', n=x, since=y, lang='en')
   tw_df <- twListToDF(tweets)
   return(tw_df)
 }
 
-# Build unit test
+# Tests if non-null output is returned using sample data
+test_return_val = function(){
+  one_pass_outcome = add_tweets(1, '2017-03-10')
+  stopifnot(!is.null(one_pass_outcome))
+  flog.info("Unit test passed!", name = "fp9")
+}
 
 if(interactive()){
   
@@ -53,6 +60,7 @@ if(interactive()){
   flog.info("Script created by: John Senft", name = "fp9")
   
   # Run unit test
+  test_return_val()
   
   ## --------------------------- Process ufo sighting data from NUFORC ------------------------------
   
@@ -76,8 +84,10 @@ if(interactive()){
   state_counts$Abbreviation <- NULL
   state_counts$region <- state_counts$State
   state_counts$State <- NULL
-  barplot(state_counts$count, las = 2, names.arg = state_counts$region, col = c(2, 3),
+  ufo_barplot <- barplot(state_counts$count, las = 2, names.arg = state_counts$region, col = c(2, 3),
           ylab = "Number of Sightings")
+  text(ufo_barplot, state_counts$count, labels = format(state_counts$count, 4),
+       pos = 3, cex = .4)
   
   # Plot map of states lit up by sightings per state
   us_map <- map_data("state")
@@ -89,38 +99,19 @@ if(interactive()){
   gg <- gg + geom_map(data=counts_map, map=us_map,
                       aes(fill=counts_map$count, map_id=region),
                       color="#ffffff", size=0.15)
-  gg <- gg + scale_fill_continuous(low='#d6f1fc', high='darkblue', 
-                                   guide='colorbar')
-  gg <- gg +theme_bw()  + labs(fill = "Sightings per state"
-                                ,title = "Heatmap of UFO Sightings Reported since 1949", x="", y="")
+  gg <- gg + scale_fill_continuous(low='#d6f1fc', high='darkblue', guide='colorbar')
+  gg <- gg +theme_bw()  + labs(fill = "Sightings per state", x="", y="")
   gg + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border = element_blank())
   
-  # Read in population data and clean up data
+  # Read in 2010 census population data and get total state populations
   pop2010_totals <- read_excel("src/12s0016.xls", sheet = 1, skip = 6, col_names = FALSE)
   pop2010_totals <- data.table(pop2010_totals)
-  cols <- c("region", "population")
   pop2010_totals <- pop2010_totals[, .(X1, X2 * 1000)]
-  names(pop2010_totals) <- cols
+  names(pop2010_totals) <- c("region", "population")
   pop2010_totals <- pop2010_totals[complete.cases(pop2010_totals),]
-  pop2010_totals <- pop2010_totals[c(6:13, 15:56)] # Remove all but states
+  pop2010_totals <- pop2010_totals[c(6:13, 15:56)] # Remove non-state rows
   pop2010_totals$region <- trim.trailing(pop2010_totals$region)
   pop2010_totals$region <- tolower(pop2010_totals$region)
-  
-  # Want sightings as a percentage of state population since 2010
-  ufo_db_us$datetime <- as.Date(ufo_db_us$datetime, format="%m/%d/%Y")
-  ufo_db_us_2010 <- subset(ufo_db_us, ufo_db_us$datetime >= "2010-01-01")
-  state_counts_2010 <- ufo_db_us_2010[, .(count = .N), by = state]
-  state_counts_2010$state <- toupper(state_counts_2010$state)
-  state_counts_2010$Abbreviation <- state_counts_2010$state
-  state_counts_2010$state <- NULL
-  states_2010 <- merge(state_counts_2010, state_names, by="Abbreviation")
-  states_2010$State <- tolower(states_2010$State)
-  states_2010$Abbreviation <- NULL
-  states_2010$region <- states_2010$State
-  states_2010$State <- NULL
-  ufo_pop_merge <- merge(states_2010, pop2010_totals, by="region")
-  ufo_pctpop_2010 <- ufo_pop_merge[, .(pct_pop = count/population * 100), by = region]
-  ##--------------------------------------------------------------------------------------
   
   # Want sightings as a percentage of state population since 2010
   ufo_db_us$datetime <- as.Date(ufo_db_us$datetime, format="%m/%d/%Y")
@@ -146,31 +137,37 @@ if(interactive()){
   gg2010 <- gg2010 + geom_map(data=pctpop_2010_map, map=us_map,
                       aes(fill=pctpop_2010_map$pct_pop, map_id=region),
                       color="#ffffff", size=0.15)
-  gg2010 <- gg2010 + scale_fill_continuous(low='#F5D9D0', high='darkred', 
-                                   guide='colorbar')
-  gg2010 <- gg2010 +theme_bw()  + labs(fill = "Per Capita Sightings"
-                               ,title = "Heatmap of UFO Sightings as Percent of Population since 2010"
-                               , x="", y="")
-  gg2010 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c())
-                                          + theme(panel.border = element_blank())
+  gg2010 <- gg2010 + scale_fill_continuous(low='#F5D9D0', high='darkred', guide='colorbar')
+  gg2010 <- gg2010 +theme_bw()  + labs(fill = "Per Capita Sightings", x="", y="")
+  gg2010 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border = element_blank())
   
   ## -------------------------------- Process twitter data ----------------------------------------
   
-  # Setup Twitter OAuth
+  # Load Twitter credentials
   tw_creds <- load_my_csv('creds/tw_creds_JJS.csv')
   TWITTER_ACCESS_TOKEN <- tw_creds$TWITTER_ACCESS_TOKEN
   TWITTER_ACCESS_SECRET <- tw_creds$TWITTER_ACCESS_SECRET
-  setup_twitter_oauth(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
   
   # Grab new tweets and append to aggregate, clean up
-  tw_df_ufo_more <- add_tweets('2017-03-11')
+  tw_df_ufo_more <- add_tweets(2000, '2017-03-12')
   tw_df_ufo_more$text <- sapply(tw_df_ufo_more$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
   tw_full_df <- rbind(tw_full_df, tw_df_ufo_more)
   tw_full_df <- subset(tw_full_df, !duplicated(tw_full_df$text))
   
-  # Replace state abbr & state names with spaces
+  # Replace state abbr with state names, then remove the space in the 10 names that have one
+  i <- 1
+  for(i in seq(tw_full_df$text)){
+    tw_full_df$text[[i]] <- gsub(" ME ", " maine ", tw_full_df$text[[i]])
+    tw_full_df$text[[i]] <- gsub(" IN ", " indiana ", tw_full_df$text[[i]])    
+    tw_full_df$text[[i]] <- gsub(" OH ", " ohio ", tw_full_df$text[[i]])
+    tw_full_df$text[[i]] <- gsub(" OK ", " oklahoma ", tw_full_df$text[[i]])
+    tw_full_df$text[[i]] <- gsub(" OR ", " oregon ", tw_full_df$text[[i]])
+  }
+  
+  # Replace state abbr with state names and remove the space in the 10 names that have one
   j <- 1
-  for(j in seq(tw_full_df$text)){   
+  for(j in seq(tw_full_df$text)){
+    tw_full_df$text[[j]] <- gsub('([[:upper:]])', '\\L\\1', tw_full_df$text[[j]], perl = TRUE)
     tw_full_df$text[[j]] <- gsub("north carolina", " northcarolina ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub("north dakota", " northdakota ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub("new hampshire", " newhampshire ", tw_full_df$text[[j]])
@@ -205,13 +202,11 @@ if(interactive()){
     tw_full_df$text[[j]] <- gsub(" ia ", " iowa ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" id ", " idaho ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" il ", " illinois ", tw_full_df$text[[j]])
-    tw_full_df$text[[j]] <- gsub(" In ", " indiana ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" ks ", " kansas ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" ky ", " kentucky ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" la ", " louisiana ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" ma ", " massachusetts ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" md ", " maryland ", tw_full_df$text[[j]])
-    tw_full_df$text[[j]] <- gsub(" Me ", " maine ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" mi ", " michigan ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" mn ", " minnesota ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" mo ", " missouri ", tw_full_df$text[[j]])
@@ -219,9 +214,6 @@ if(interactive()){
     tw_full_df$text[[j]] <- gsub(" mt ", " montana ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" ne ", " nebraska ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" nv ", " nevada ", tw_full_df$text[[j]])
-    tw_full_df$text[[j]] <- gsub(" oh ", " ohio ", tw_full_df$text[[j]])
-    tw_full_df$text[[j]] <- gsub(" ok ", " oklahoma ", tw_full_df$text[[j]])
-    tw_full_df$text[[j]] <- gsub(" Or ", " oregon ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" pa ", " pennsylvania ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" tn ", " tennessee ", tw_full_df$text[[j]])
     tw_full_df$text[[j]] <- gsub(" tx ", " texas ", tw_full_df$text[[j]])
@@ -251,7 +243,7 @@ if(interactive()){
   tw_corpus_matrix = as.matrix(dtm)
   tw_words_df = as.data.frame(tw_corpus_matrix)
   
-  # Search tweets for state matches and sum counts for each state
+  # Search term matrix for state name matches
   search_states = c()
   search_states = gsub("[[:space:]]", "", states$region)
   tw_freq_words = colSums(tw_corpus_matrix)
@@ -260,6 +252,7 @@ if(interactive()){
   names(tw_freq_words) = c("region", "count")
   words_states <- tw_freq_words[region %in% search_states, .(region, count)]
   
+  # Modify state names by adding space back into the 10 names needing it for mapping
   k <- 1
   for (k in seq(words_states$region)){
     words_states$region[k] <- gsub("northcarolina", "north carolina", words_states$region[k])
@@ -274,18 +267,32 @@ if(interactive()){
     words_states$region[k] <- gsub("westvirginia", "west virginia", words_states$region[k])
   }
   
-  # Need to add back in states that have 0 tweet count
-  missing_states = data.frame('region'= c("new hampshire", "north carolina", 
-                                           "south dakota", "new jersey", "rhode island"),
-                              'count'= rep(0, 5))
-  words_states = rbind(words_states, missing_states) 
-  
   # Want to merge in ratio of sightings from total sightings for each state
-  sighting_count <- sum(ufo_db_us_2010$count)
-  ufo_pct_count <- ufo_db_us_2010[, .(pct_count = count/sighting_count), by = region]
+  ufo_pct_count <- ufo_db_us_2010[, .(pct_count = count/sum(ufo_db_us_2010$count)), by = region]
   words_states <- merge(words_states, ufo_pct_count, by="region")
-  barplot(words_states$count, las = 2, names.arg = words_states$region, col = c(2, 3),
+  
+  # Look at twitter count barplot
+  tw_barplot <- barplot(words_states$count, las = 2, names.arg = words_states$region, col = c(2, 3),
           ylab = "Number of Sightings")
+  text(tw_barplot, words_states$count, labels = format(words_states$count, 4),
+       pos = 3, cex = .4)
+  
+  # Look at twitter count on us map graphic
+  tweet_map <- merge(us_map, words_states, by="region")
+  ggtw <- ggplot()
+  ggtw <- ggtw + geom_map(data=us_map, map=us_map,
+                      aes(x=long, y=lat, map_id=region),
+                      fill="#ffffff", color="#ffffff", size=0.15)
+  ggtw <- ggtw + geom_map(data=tweet_map, map=us_map,
+                      aes(fill=tweet_map$count, map_id=region),
+                      color="#ffffff", size=0.15)
+  ggtw <- ggtw + scale_fill_continuous(low='#c5f9d0', high='darkgreen', guide='colorbar')
+  ggtw <- ggtw +theme_bw()  + labs(fill = "Sightings per state", x="", y="")
+  ggtw + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border = element_blank())
+  
+  # Look at basic stats
+  ufo_mean <- mean(ufo_db_us_2010$count)
+  tw_mean <- mean(words_states$count)
   
   # Look at chi-sq stats
   # H0: NUFORC sightings are a pridictor of twitter ufo posts.
@@ -293,7 +300,6 @@ if(interactive()){
   ufo_cs_test <- chisq.test(words_states$count, p = words_states$pct_count)
   
   # Log results
-  flog.info("Chi-Squared test results are as follows:", ufo_cs_test, capture=T, name="fp9")
-  
+  flog.info("UPDATED Chi-Squared test results are as follows:", ufo_cs_test, capture=T, name="fp9")
 }
 ##--------------------------------End of Script----------------------------------------
